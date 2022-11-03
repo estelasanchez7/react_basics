b1bc91d1d4589d7be8cdc415fc2ace507704b617
estelasanchez7 <stelasv7@gmail.com>
2022-11-03 21:31:26 +0100

    Propiedades-props-proptype

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Componente from './components/Componente'
+import Propiedades from './components/Propiedades';
 import React from 'react'; //no es necesario aquí, pero en versiones anteriores í, así que por si acaso trabajamos en otros proyectos anteriores a esta versión es conveniente ponerlo.
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     
     <div className="App">
       <header className="App-header">
+        <section>
         <img src={logo} className="App-logo" alt="logo" />
           Edit <code>src/App.js</code> and save to reload.
         <a
@@ -18,9 +20,22 @@ function App() {
         >
           Learn React
         </a>
+        </section>
+
       </header>
       <section>
         <Componente msg='Hola soy un componente funcional desde una arrow function desde una prop'/>
+        <hr/>
+        <Propiedades
+        cadena='Esto es una cadena de texto'
+        numero={19}
+        booleano={true}
+        arreglo={[123,5,645]}
+        objeto={{nombre:'Jon',correo:'jonmircha@gmail.com'}}
+        funcion={(num)=>num*num}
+        elementoReact={<i>Esto es un elemento React</i>}
+        elementoComponenteReact={<Componente msg='soy un componente pasado como prop'/>}
+        />
         <br/>
         <br/>
         <br/>
