a499ed5c0490d828b78d7ea9243ef717b1f8bf30
estelasanchez7 <stelasv7@gmail.com>
2022-11-03 19:42:08 +0100

    components

--- src/App.js
+++ src/App.js
@@ -4,27 +4,12 @@ import Componente from './components/Componente'
 import React from 'react'; //no es necesario aquí, pero en versiones anteriores í, así que por si acaso trabajamos en otros proyectos anteriores a esta versión es conveniente ponerlo.
 
 function App() {
-  let nombre='Estela'
-  let auth = false
-  let estaciones = ['Primavera', 'Verano', 'Otoño', 'Invierno']
   return (
-    <>
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <label htmlFor='nombre'>Nombre</label>
-        <input type='text' id='nombre'/>
-        <h1>{nombre}</h1>
-        <p>{auth ? 'El usuario está logueado' : 'El usuario no está logueado'}</p>
-        <p>
-          Editar <code>src/App.js</code> and save to reload.
-        </p>
-        <p>{2+1}</p>
-        <ul>
-          {estaciones.map((el, index) => (
-            <li key={index}>{el}</li>
-          ))}
-        </ul>
+          Edit <code>src/App.js</code> and save to reload.
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -34,8 +19,18 @@ function App() {
           Learn React
         </a>
       </header>
+      <section>
+        <Componente msg='Hola soy un componente funcional desde una arrow function desde una prop'/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+      </section>
     </div>
-    </>
   );
 }
 
--- src/components/Componente.js
+++ src/components/Componente.js
@@ -1,9 +1,15 @@
 import React, {Component} from "react";
 
-class Componente extends Component{
+const Componente = (props) => <h2>{props.msg}</h2>
+
+/*function Componente(props){
+    return <h2>{props.msg}</h2>
+}*/
+
+/*class Componente extends Component{
     render(){
-        return <h2>Hola soy un componente</h2>
+        return <h2>{this.props.msg}</h2>
     }
-}
+}*/
 
 export default Componente
