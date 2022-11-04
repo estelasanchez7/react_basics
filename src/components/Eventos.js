09936934f4214f368875ebceb085b71cc49c6e48
estelasanchez7 <stelasv7@gmail.com>
2022-11-04 16:30:47 +0100

    Eventos_properties_initializer

--- src/components/Eventos.js
+++ src/components/Eventos.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 
-export default class Eventos extends Component{
+//ES6
+export class EventosES6 extends Component{
     constructor(props){
         super(props)
         this.state={
@@ -31,7 +32,46 @@ export default class Eventos extends Component{
     render(){
         return(
            <div>
-            <h2>Eventos en Componentes de Clase</h2>
+            <h2>Eventos en Componentes de Clase ES6</h2>
+            <nav>
+                <button onClick={this.sumar}>+</button>
+                <button onClick={this.restar}>-</button>
+            </nav>
+            <h3>{this.state.contador}</h3>
+           </div> 
+        )
+    }
+}
+
+
+//ES7 -> Properties Initializer
+export class EventosES7 extends Component{
+        state={
+            contador:0,
+        }
+
+    //Arrow functions    (heredan el this de la clase a la que pertenecen)
+    sumar=(e)=>{
+        console.log('Sumando')
+        console.log(this)
+        this.setState({
+            contador:this.state.contador+1
+        })
+    }
+
+    restar=(e)=>{
+        console.log('Restando')
+        console.log(this)
+        this.setState({
+            contador:this.state.contador-1
+        })
+    }
+
+
+    render(){
+        return(
+           <div>
+            <h2>Eventos en Componentes de Clase ES7</h2>
             <nav>
                 <button onClick={this.sumar}>+</button>
                 <button onClick={this.restar}>-</button>
