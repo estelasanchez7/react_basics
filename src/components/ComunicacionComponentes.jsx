dbae7dd29017868cb4b87d952e453777964eb5d7
estelasanchez7 <stelasv7@gmail.com>
2022-11-07 12:16:16 +0100

    ComunicacionComponentes

---
+++ src/components/ComunicacionComponentes.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+export default class Padre extends Component {
+  state = {
+    contador: 0,
+  };
+
+  incrementarContador = (e) => {
+    this.setState({
+      contador: this.state.contador + 1,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <h2>Comunicación entre componentes</h2>
+        <p>Contador: <b>{this.state.contador}</b></p>
+        <Hijo mensaje="Mensaje para el hijo 1"     incrementarContador={this.incrementarContador}
+        />
+        
+        <Hijo
+          incrementarContador={this.incrementarContador}
+          mensaje="Mensaje para el hijo 2"
+        />
+      </>
+    );
+  }
+}
+
+function Hijo(props) {
+  return(
+    <>
+    <h2>{props.mensaje}</h2>
+    <button onClick={props.incrementarContador}>+</button>
+    </>
+  )
+}
