1ffb14269228d7b09a1153f7dbc6f5f8f25d61c7
estelasanchez7 <stelasv7@gmail.com>
2022-11-08 22:10:12 +0100

    Hook - useEffect

---
+++ src/components/ScrollHooks.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+
+export default function ScrollHooks() {
+    const [scrollY, setScrollY] = useState(0)
+   
+
+  useEffect(() => {
+    console.log("Haciendo Scroll");
+
+    const detectarScroll =()=> setScrollY(window.pageYOffset)
+
+    window.addEventListener('scroll',detectarScroll)
+
+    return()=>{
+        window.removeEventListener('scroll',detectarScroll)
+        console.log("Fase de Desmontaje")
+    }
+    
+  },[scrollY]); //solo se va a ejecutar cuando la variable scrollY tenga cambios
+
+
+  useEffect(() => {
+    console.log("Fase de Montaje")
+  },[]);
+
+  useEffect(() => {
+    console.log("Fase de Actualización")
+  });
+
+  useEffect(() => { 
+    return()=> {
+        console.log("Fase de Desmontaje")
+    }
+  });
+
+
+
+  return (
+    <>
+      <h2>Hooks - useEffect y el Ciclo de Vida</h2>
+      <p>Scroll Y del Navegador {scrollY}px</p>
+    </>
+  );
+}
