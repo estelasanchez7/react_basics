4689d1f1bda68f85344f43a5086adc4e14c3cfa5
estelasanchez7 <stelasv7@gmail.com>
2022-11-14 11:57:43 +0100

    Estilos

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import AjaxHooks from './components/AjaxHooks';
 import HooksPersonalizados from './components/HooksPersonalizados';
 import Referencias from './components/Referencias';
 import Formularios from './components/Formularios';
+import Estilos from './components/Estilos';
 
 function App() {
   return (
@@ -81,6 +82,8 @@ function App() {
           <Referencias/>
           <hr />
           <Formularios/>
+          <hr />
+          <Estilos/>
           <br /><br /><br /><br /><br /><br /><br />
         </section>
       </header>
